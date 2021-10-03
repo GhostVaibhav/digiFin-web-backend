ca6c61fbf6ef136d734db93d7d4e01227003f641
kri5h92 <krishgjain9@gmail.com>
2021-10-03 14:05:41 +0530

    API Completed with delete route issue

--- controllers/transactionData.js
+++ controllers/transactionData.js
@@ -19,7 +19,7 @@ const showTransaction = (async(req,res) => {
 
         const transaction = await Transaction.find({userId: userId, transactionDate : {$gte: new Date(new Date(startDate).setHours(00, 00, 00)), $lt: new Date(new Date(endDate).setHours(23, 59, 59))} })
 
-        const totalAmount = 0    
+        let totalAmount = 0   
         for(var i = 0; i < transaction.length; i++)
         {
             totalAmount += transaction[i].transactionAmount
@@ -33,7 +33,7 @@ const showTransaction = (async(req,res) => {
                 })
             }
 
-        return res.status(200).json({status: 'success',data: transaction,total: totalAmount})
+        return res.status(200).json({status: 'success',data: transaction,total: totalAmount, count: transaction.length})
     }
     catch(error) 
     {
@@ -52,7 +52,7 @@ const showTransactionAll = (async(req,res) => {
 
         const transaction = await Transaction.find({userId: userId})
     
-        const totalAmount = 0    
+        let totalAmount = 0    
         for(var i = 0; i < transaction.length; i++)
         {
             totalAmount += transaction[i].transactionAmount
@@ -66,7 +66,7 @@ const showTransactionAll = (async(req,res) => {
             })
         }
 
-        return res.status(200).json({status: 'success',data: transaction,total: totalAmount})
+        return res.status(200).json({status: 'success',data: transaction,total: totalAmount, count: transaction.length})
     }
     catch(error) 
     {
@@ -92,7 +92,7 @@ const showTransactionParticular = (async(req,res) => {
             })
         }
 
-        return res.status(200).json({status: 'success',data: transaction,total: totalAmount})
+        return res.status(200).json({status: 'success',data: transaction,total: transaction.transactionAmount})
     }
     catch(error) 
     {
@@ -105,26 +105,80 @@ const showTransactionParticular = (async(req,res) => {
 
 const addTransaction = (async(req,res) => {
 
-    const{userId, transactionDate, transactionId, transactionAmount} = req.body
+    try
+    {
+        const{userId, transactionDate, transactionId, transactionAmount} = req.body
 
-    const transaction = new Transaction({
-        userId, transactionDate, transactionId, transactionAmount
-    })
-    try{
+        const transaction = new Transaction({
+            userId, transactionDate, transactionId, transactionAmount
+        })
+
+        transaction.transactionDate instanceof Date
+        
         await transaction.save()
-    }catch(err){
-        return res.status(500).send(err)
+        return res.status(201).json({status: 'record successfully added',data: transaction})
     }
-    return res.status(201).send(transaction)
-
+    catch(error) 
+    {
+        return res.status(500).json({
+            status:'failure',
+            error: error.message
+        })
+    }
 })
 
 const updateTransaction = (async(req,res) => {
+    try{
+        const {transactionid} = req.query
 
+        const{userId, transactionDate, transactionId, transactionAmount} = req.body
+
+        if(transactionId != transactionid)
+        { 
+            return res.status(404).json({
+                status: 'failure',
+                message: 'enter valid transactionId'
+            })
+        }
+
+
+        const transaction = await Transaction.findOneAndUpdate({transactionId: transactionid},
+        {
+            userId, transactionDate, transactionId, transactionAmount
+        },{new: true})
+        if(!transaction || transaction.length === 0)
+        { 
+            return res.status(404).json({
+                status: 'failure',
+                message: 'could not retrieve any records'
+            })
+        }
+        await transaction.save()
+        return res.status(201).json({status: 'record successfully updated',data: transaction})
+    }catch(error) 
+    {
+        return res.status(500).json({
+            status:'failure',
+            error: error.message
+        })
+    }
 })
 
 const deleteTransaction = (async(req,res) => {
-
+    try{
+        transactionId = req.query
+        await Transaction.findOneAndDelete({transactionId: transactionId})
+    }
+    catch(error) 
+    {
+        return res.status(500).json({
+            status:'failure',
+            error: error.message
+        })
+    }
+    return res.status(202).json({
+        status: 'record successfully deleted',
+    })
 })
 
 module.exports = {showTransaction, showTransactionAll, updateTransaction, deleteTransaction, addTransaction, testRoute, showTransactionParticular}
